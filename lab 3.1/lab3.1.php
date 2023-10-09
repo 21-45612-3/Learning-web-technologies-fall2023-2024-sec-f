a6cd32bf8c615e01ae1b54961dfdd7097d702b59
21-45612-3 <139360630+21-45612-3@users.noreply.github.com>
2023-10-09 15:41:48 +0600

    Add files via upload

---
+++ lab 3.1/lab3.1.php
@@ -0,0 +1,23 @@
+<?php 
+print_r($_GET);
+echo $_GET['name'];
+
+print_r($_GET);
+echo $_GET['Email'];
+
+
+print_r($_GET);
+echo $_GET['gender'];
+
+
+print_r($_GET);
+echo $_GET['dob'];
+
+print_r($_GET);
+echo $_GET['blood'];
+
+
+print_r($_GET);
+echo $_GET['degree'];
+
+?>
