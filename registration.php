549edcb28c8cf26c3ccd8e5e8a3a330d2ad42421
21-45612-3 <139360630+21-45612-3@users.noreply.github.com>
2023-10-14 16:46:58 +0600

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,146 @@
+<html>
+<head>
+</head>
+<body>
+
+
+
+    <table border="1" width="50%">
+        <tr>
+            <td colspan="3">
+                <h1><center>Personal Profile</center><h1>
+            </td>
+
+        <tr>
+
+            <td>
+
+                Name
+
+            </td>
+            <td>
+                <input type="text" name="" value="">
+            </td>
+
+        </tr>
+
+
+        <tr>
+            <td>
+
+                Email
+
+            </td>
+            <td>
+                <input type="text" name="" value="">
+            </td>
+
+        </tr>
+
+
+
+        <tr>
+            <td>
+
+                Gender
+
+            </td>
+            <td>
+                <input type="radio" name="gerder" value=""> male
+                <input type="radio" name="gerder" value=""> female
+                <input type="radio" name="gerder" value=""> other
+            </td>
+
+        </tr>
+
+        <tr>
+            <td>
+
+                Date of Birth
+
+            </td>
+            <td>
+                <input type="date" name="" value="">dd/mm/yyyy
+            </td>
+
+        </tr>
+
+
+
+
+        <tr>
+            <td>
+
+                Blood Group
+
+            </td>
+            <td>
+
+                <select>
+                    <option>A+</option>
+                    <option>B+</option>
+                    <option>AB+</option>
+                    <option>O+</option>
+
+
+                </select>
+
+            </td>
+
+        </tr>
+
+
+
+        <tr>
+            <td>
+
+                Degree
+
+            </td>
+            <td>
+                <input type="checkbox" name="" value=""> SSC
+                <input type="checkbox" name="" value=""> HSC
+                <input type="checkbox" name="" value=""> BSc
+                <input type="checkbox" name="" value=""> MSc
+            </td>
+
+        </tr>
+
+
+
+        <tr>
+            <td>
+
+                Photo
+
+            </td>
+            <td>
+                <input type="file" name="" value="">
+            </td>
+
+        </tr>
+
+
+        <tr>
+            <td colspan="3">
+                <br />
+                <br />
+            </td>
+
+        <tr>
+
+        <tr>
+            <td colspan="3" align="right">
+                <br />
+                <input type="submit" name="" value="submit">
+                <input type="reset" name="" value="reset">
+            </td>
+
+        <tr>
+
+    </table>
+
+
+</body>
+
+</html>
